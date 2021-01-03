3464113367db89f2ce5f4658e0ad69af8cabc92a
gabrielrondon <grondon@gmail.com>
2021-01-03 22:11:05 +0100

    js

--- index.js
+++ index.js
@@ -1,42 +1,9 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Bootstrap 4 generator example page</title>
-    <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <meta name="description" content="">
-      <meta name="author" content="">
-      <link rel="icon" href="favicon.ico">
-      <link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.1.3/sketchy/bootstrap.min.css ">
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
+const http = require('http')
+const fs = require('fs')
 
-      <!-- Plugins -->
-    </head>
-    <body>
-      <section class="pt-5 pb-5">
-        <div class="container pb-5 pt-5">
-          <div class="row align-items-center justify-content-between ">
-            <div class="col-12 col-md-5 pr-md-5 ">
-              <h1 class="mb-3 mt-5 display-4 font-weight-bold">WiseReader</h1>
-              <p class="lead  ">smartblocks.</p>
-              <div class="  d-flex mt-3 mb-1">
-                <a class="btn btn-success btn-lg  mt-md-3  " href="#" role="button">new version</a>
-              </div>
-            </div>
-            <div class="col-12 col-md-7  mt-4 mt-md-0">
-              <div class="embed-responsive embed-responsive-16by9 shadow-lg rounded overflow-hidden">
-                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/I1XrwNNtGoU?rel=0" allowfullscreen=""></iframe>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('index.html').pipe(res)
+})
 
-      <!-- jQuery is required -->
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js"></script>
-    </body>
-</html>
+server.listen(process.env.PORT || 3000)
